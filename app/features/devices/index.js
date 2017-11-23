cee565c6ab06ccae54d9c2fd76eee9e2d3b59584
Javier Diaz <coderdiaz@gmail.com>
2017-11-23 01:45:51 -0600

    Added devices feature

---
+++ app/features/devices/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var express = require('express')
+var router = express.Router()
+var bodyParser = require('body-parser')
+
+router.use(bodyParser.urlencoded({ extended: true }))
+
+module.exports = router
