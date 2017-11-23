82412c6c0b6e5a07f186d657db4ffbf7829ae353
Javier Diaz <coderdiaz@gmail.com>
2017-11-23 01:35:07 -0600

    Custom server name

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
 'use strict'
 
-var app = require('./server')
+var server = require('./server')
 var port = process.env.PORT || 3000
 
-app.listen(port, function () {
+server.listen(port, function () {
   console.log('API is running on port ' + port)
 })
