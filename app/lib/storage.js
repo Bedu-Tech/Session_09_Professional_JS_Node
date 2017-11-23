209fcbf630e489664b066d0e0b9b5d4340581d86
Javier Diaz <coderdiaz@gmail.com>
2017-11-23 13:03:42 -0600

    Added new function to storage users

--- app/lib/storage.js
+++ app/lib/storage.js
@@ -15,6 +15,46 @@ var getUsers = function () {
 }
 
 /**
+ * Function to known if user exist in storage
+ * @param {String} userName
+ * @return Boolean
+ */
+var userExist = function (userName) {
+  var exist = false
+  var users = persist.getItemSync('users')
+  if (typeof users == 'undefined') {
+    users = []
+  }
+  users.map(function (user) {
+    if (user.username === userName) {
+      exist = true
+    }
+  })
+
+  return exist
+}
+
+/**
+ * Function to get user by username
+ * @param {String} userName
+ * @return Object
+ */
+var userFind = function (userName) {
+  var matchedAccount
+  var users = persist.getItemSync('users')
+  if (typeof users == 'undefined') {
+    users = []
+  }
+  users.map(function (user) {
+    if (user.username === userName) {
+      matchedAccount = user
+    }
+  })
+
+  return matchedAccount
+}
+
+/**
  * Function for save user to storage
  * @param {Array} users
  * @param {Object} newUser
@@ -26,7 +66,34 @@ var saveUser = function (users, newUser) {
   return users
 }
 
+/**
+ * Function to get devices from storage
+ * @return Array
+ */
+var getDevices = function () {
+  var devices = persist.getItemSync('devices')
+  if (typeof devices == 'undefined') {
+    devices = []
+  }
+  return devices
+}
+
+/**
+ * Function to save device to storage
+ * @param {Array} devices 
+ * @param {Object} newDevice 
+ */
+var saveDevice = function (devices, newDevice) {
+  devices.push(newDevice)
+  persist.setItemSync('devices', devices)
+  return devices
+}
+
 module.exports = {
   getUsers,
-  saveUser
+  saveUser,
+  userExist,
+  userFind,
+  getDevices,
+  saveDevice
 }
