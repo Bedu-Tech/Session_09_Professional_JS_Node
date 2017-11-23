fe3614683e35062be45e1819638d555a67eb41a9
Javier Diaz <coderdiaz@gmail.com>
2017-11-23 13:03:08 -0600

    Added logic for devices

--- app/features/devices/index.js
+++ app/features/devices/index.js
@@ -3,8 +3,57 @@
 var express = require('express')
 var router = express.Router()
 var bodyParser = require('body-parser')
+var {storage} = require('../../lib')
+var uuid = require('uuid/v4')
 
 router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({ extended: true }))
+
+router.get('/', function (req, res) {
+  var devices = storage.getDevices()
+  res.json({
+    data: devices
+  })
+})
+
+router.post('/new/:name', function (req, res) {
+  if (!req.body.username) {
+    res.status(400).send({
+      statusCode: 400,
+      error: 'Bad request',
+      message: 'Not username provided to link smartlock'
+    })
+  }
+  var userExist = storage.userExist(req.body.username)
+
+  if (!userExist) {
+    res.status(400).send({
+      statusCode: 400,
+      error: 'Bad request',
+      message: 'Username provided not exist'
+    })
+  }
+
+  var userName = req.body.username
+  var deviceName = req.params.name
+  var devices = storage.getDevices()
+  var newDevice = {
+    _id: uuid(),
+    name: deviceName,
+    accessTo: [
+      {
+        user: userName
+      }
+    ],
+    createdAt: new Date(),
+    modifiedAt: new Date()
+  }
+  storage.saveDevice(devices, newDevice)
+  res.json({
+    success: true,
+    messages: 'Devices created successfully',
+    device: newDevice
+  })
+})
 
 module.exports = router
