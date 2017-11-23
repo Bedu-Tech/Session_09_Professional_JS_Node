78badd250e4ead0850fb1381a2919901fee7a15c
Javier Diaz <coderdiaz@gmail.com>
2017-11-23 11:57:37 -0600

    Added body parser json to devices feature

--- app/features/devices/index.js
+++ app/features/devices/index.js
@@ -4,6 +4,7 @@ var express = require('express')
 var router = express.Router()
 var bodyParser = require('body-parser')
 
+router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({ extended: true }))
 
 module.exports = router
