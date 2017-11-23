3816c9f4fc2c27843b6c17e2e62f132ebf0775ff
Javier Diaz <coderdiaz@gmail.com>
2017-11-23 01:45:56 -0600

    Added features to server

--- app/server/index.js
+++ app/server/index.js
@@ -1,6 +1,14 @@
 'use strict'
 
+// Instance for express
 var express = require('express')
 var app = express()
+
+// Features
+var users = require('../features/users')
+var devices = require('../features/devices')
+
+app.use('/users', users)
+app.use('/devices', devices)
 
 module.exports = app
